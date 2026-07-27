a54febbbc985f6154a0bf56e8f51eef46e7369d7
ZerAp001 <pachecovargas124@gmail.com>
2026-07-27 12:25:45 -0600

    corregir conexion a PDO

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,11 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
-$port = getenv('DB_PORT');
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'identiband';
+$port = '3306';
 
 try {
-    // Esta es la forma profesional de conectar sin depender de 'mysqli'
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
     $conexion = new PDO($dsn, $user, $pass);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
